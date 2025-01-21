929c24aab19f20b85fa61dd6cc4c7d50d211ab45
Anuar Zhangeldi <anuar1202@gmail.com>
2025-01-21 17:29:09 +0500

    edited pageobjects

--- src/test/java/pages/BeginnersPage.java
+++ src/test/java/pages/BeginnersPage.java
@@ -7,10 +7,28 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class BeginnersPage {
 
-    SelenideElement mainTitle = $("div#b1-firstscreen span.H-title-for-pages-hr");
+    SelenideElement mainTitleLocator = $("div#b1-firstscreen span.H-title-for-pages-hr"),
+                    beginCareerButton = $("button.btn-search-main.OSFillParent"),
+                    moreInCareerButton = $("button.btn-search-exp-itbig"),
+                    secondaryTextLocator = $("div[data-container] span:nth-of-type(2)");
 
     public BeginnersPage checkMainTitle(String title) {
-        mainTitle.shouldHave(text(title));
+        mainTitleLocator.shouldHave(text(title));
+        return this;
+    }
+
+    public BeginnersPage clickBeginCareer() {
+        beginCareerButton.click();
+        return this;
+    }
+
+    public BeginnersPage clickMoreButtonInCareer() {
+        moreInCareerButton.click();
+        return this;
+    }
+
+    public BeginnersPage verifyTextPage(String secondaryText) {
+        secondaryTextLocator.shouldHave(text(secondaryText));
         return this;
     }
 
--- src/test/java/pages/InternshipPage.java
+++ src/test/java/pages/InternshipPage.java
@@ -1,26 +1,54 @@
 package pages;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$$;
 
 
 public class InternshipPage {
 
-    private SelenideElement
-            checkAllPositionButton = $(".btn-search-main");
+    SelenideElement
+            checkAllPositionButton = $(".btn-search-main"),
+            frequentQuestionsLocator = $("div#b1-answer div.H-title-for-pages-hr span");
+
+    ElementsCollection
+            questionsCollection = $$("div.card-arrow"),
+            answersCollection = $$("div.hidden.answer-container"),
+            jobTitles =  $$(".job-title");
 
     public InternshipPage searchAllPosition() {
         checkAllPositionButton.click();
         return this;
     }
 
-    public InternshipPage verifyPositionsWithInternship(String positionTitle){
-            $$(".job-title")
-                    .filterBy(text(positionTitle))
-                    .forEach(title -> title.shouldBe(visible));
+    public InternshipPage verifyPositionsWithInternship(String positionTitle) {
+        jobTitles
+                .filterBy(text(positionTitle))
+                .forEach(title -> title.shouldBe(visible));
+        return this;
+    }
+
+    public InternshipPage verifyTextForFrequentQuestions(String frequentQuestions) {
+        frequentQuestionsLocator.shouldBe(visible)
+                .shouldHave(text(frequentQuestions));
+        return this;
+    }
+
+    public InternshipPage clickOnQuestionFromList(String questionText) {
+        questionsCollection
+                .findBy(text(questionText))
+                .click();
+        return this;
+    }
+
+    public InternshipPage verifyDisplayedAnswer(String expectedAnswer) {
+        answersCollection
+                .findBy(text(expectedAnswer))
+                .shouldBe(visible);
         return this;
     }
 }
